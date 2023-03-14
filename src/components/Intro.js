5dce0b08c989e12c3669df072bced34683c2b2c6
Meyzan16 <Meyzan1605@gmail.com>
2023-03-14 23:42:45 +0700

    first commit

---
+++ src/components/Intro.js
@@ -0,0 +1,16 @@
+
+
+function Intro() {
+  return (
+    <div>
+      <header>
+        <nav>Testing</nav>
+      </header>
+      <section>
+        <h1>Hello Brader Sesi 1</h1>
+      </section>
+    </div>
+  );
+}
+
+export default Intro;
